10197081efcf4128342617fba8503e4124726350
Abe101 <cyrusrohail101@gmail.com>
2022-09-30 03:54:14 +0500

    Copy all images and setup boilerplate

---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import dirtImg from "./dirt.jpeg";
+import grassImg from "./grass.jpeg";
+import glassImg from "./glass.png";
+import logImg from "./log.png";
+import woodImg from "./wood.png";
+
+const assets = { dirtImg, grassImg, glassImg, logImg, woodImg };
+
+export default assets;
