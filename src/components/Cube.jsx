d6cbcf13ba34892fe0a49e531a341d7f31615701
Abe101 <cyrusrohail101@gmail.com>
2022-09-30 17:22:10 +0500

    Implement adding cubes and removing cubes

--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -1,17 +1,55 @@
 import { useBox } from "@react-three/cannon";
 import * as textures from "../assets/textures";
+import { useStore } from "../hooks/useStore";
 
 export default function Cube({ position, texture }) {
   const [ref] = useBox(() => ({
     type: "Static",
     position,
   }));
+  const [addCube, removeCube] = useStore((state) => [
+    state.addCube,
+    state.removeCube,
+  ]);
   const activeTexture = textures[texture + "Texture"];
-  console.log("activeTexture", activeTexture);
 
   return (
-    <mesh ref={ref}>
-      <boxBufferGeometry attach='geometry' />
+    <mesh
+      ref={ref}
+      onClick={(e) => {
+        e.stopPropagation();
+        const clickedFace = Math.floor(e.faceIndex / 2);
+        const { x, y, z } = ref.current.position;
+        console.log("clickedFace", clickedFace);
+        if (e.altKey) {
+          removeCube(x, y, z);
+          return;
+        }
+        switch (clickedFace) {
+          case 0:
+            addCube(x + 1, y, z);
+            return;
+          case 1:
+            addCube(x + 1, y, z);
+            return;
+          case 2:
+            addCube(x, y + 1, z);
+            return;
+          case 3:
+            addCube(x, y - 1, z);
+            return;
+          case 4:
+            addCube(x, y, z + 1);
+            return;
+          case 5:
+            addCube(x - 1, y, z - 1);
+            return;
+          default:
+            return;
+        }
+      }}
+    >
+      <boxGeometry attach='geometry' />
       <meshStandardMaterial map={activeTexture} attach='material' />
     </mesh>
   );
--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,15 +1,24 @@
 import { usePlane } from "@react-three/cannon";
 
 import { groundTexture } from "../assets/textures";
+import { useStore } from "../hooks/useStore";
 
 export default function Ground() {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
-    position: [0, 0, 0],
+    position: [0, -0.5, 0],
   }));
+  const [addCube] = useStore((state) => [state.addCube]);
 
   return (
-    <mesh ref={ref}>
+    <mesh
+      ref={ref}
+      onClick={(e) => {
+        e.stopPropagation();
+        const [x, y, z] = Object.values(e.point).map((val) => Math.ceil(val));
+        addCube(x, y, z);
+      }}
+    >
       <planeGeometry attach='geometry' args={[100, 100]} />
       <meshStandardMaterial attach='material' map={groundTexture} />
     </mesh>
