4597f9df43c2aaa4dc9c3d594f0130eb186f5077
Abe101 <cyrusrohail101@gmail.com>
2022-09-30 20:59:15 +0500

    Change document title and icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Physics } from "@react-three/cannon";
 import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Helmet } from "react-helmet";
 
 import Ground from "./components/Ground";
 import Player from "./components/Player";
@@ -12,6 +13,11 @@ import Menu from "./components/Menu";
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Minecraft + React + Three</title>
+        <link rel="icon" href="/icons8-minecraft-grass-cube-gradient-120.png" />
+      </Helmet>
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intesity={0.5} />
