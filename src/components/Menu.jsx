c0c2f1d1db07b1cc780d1aa0097f75b8ca555c87
Abe101 <cyrusrohail101@gmail.com>
2022-09-30 20:15:51 +0500

    Add legend

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,8 +8,22 @@ export default function Menu() {
 
   return (
     <div className='menu absolute'>
-      <button onClick={saveWorld}>Save</button>
-      <button onClick={resetWorld}>Reset</button>
+        <div>
+            <p>Controls:</p>
+            <ul>
+                <li>W: Move Forward</li>
+                <li>A: Move Left</li>
+                <li>S: Move Backward</li>
+                <li>D: Move Right</li>
+                <li>Space: Jump</li>
+                <li>Click: Place a block</li>
+                <li>Alt + Click: Remove a block</li>
+                <li>Digits 1-5: Select cube</li>
+            </ul>
+        </div>
+      <button onClick={saveWorld}>Save World</button>
+      <br />
+      <button onClick={resetWorld}>Reset World</button>
     </div>
   );
 }
