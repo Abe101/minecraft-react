cebc4e249d78430c90c29f00cd26bd25c5d7e9be
Abe101 <cyrusrohail101@gmail.com>
2022-10-02 22:18:34 +0500

    Add styling to the menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,16 +8,21 @@ export default function Menu() {
   const [isHidden, setIsHidden] = useState(false);
 
   return (
-    <div className="absolute top-3 left-3">
-      <button onClick={() => setIsHidden(!isHidden)}>
+    <div className="absolute top-3 left-3 bg-slate-400 rounded-lg p-1 m-1">
+      <button
+        className="bg-zinc-900 rounded-lg p-1 opacity-70 hover:opacity-100 hover:transition-opacity m-1"
+        onClick={() => setIsHidden(!isHidden)}
+      >
         {isHidden ? "Show Controls" : "Hide Controls"}
       </button>
-      <div className={`${isHidden ? "hidden" : ""}`}>
-        <div>
-          <p>Version: 0.1.24 Alpha</p>
-        </div>
-        <div>
-          <p>Controls:</p>
+      <div className={`${isHidden ? "hidden transition-all" : ""}`}>
+        <div className="mx-1">
+          <p>
+            <span className="font-bold">Version</span>: 0.1.24 Alpha
+          </p>
+          <p>
+            <span className="font-bold">Controls</span>:
+          </p>
           <ul>
             <li>WASD: Movement</li>
             <li>Space: Jump</li>
@@ -25,9 +30,23 @@ export default function Menu() {
             <li>Alt + Click: Remove a block</li>
             <li>Digits 1-7: Select block</li>
           </ul>
-          <button onClick={() => dispatch(saveCubes())}>Save</button>
-          <br />
-          <button onClick={() => dispatch(resetCubes())}>Reset</button>
+        </div>
+        <div>
+          <div className="flex flex-row gap-1">
+            <button
+              className="bg-zinc-900 rounded-lg p-1 opacity-70 hover:opacity-100 hover:transition-opacity m-1"
+              onClick={() => dispatch(saveCubes())}
+            >
+              Save
+            </button>
+            <br />
+            <button
+              className="bg-zinc-900 rounded-lg p-1 opacity-70 hover:opacity-100 hover:transition-opacity m-1"
+              onClick={() => dispatch(resetCubes())}
+            >
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     </div>
