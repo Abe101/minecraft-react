f9332fcaaa78885f98c2d68fd60b93d2da8c495a
Abe101 <cyrusrohail101@gmail.com>
2022-10-09 05:06:03 +0500

    Add a name div to label selected texture over textureselector

--- src/components/TextureSelector.jsx
+++ src/components/TextureSelector.jsx
@@ -27,6 +27,7 @@ export default function TextureSelector() {
   const dispatch = useDispatch();
   const [visible, setVisible] = useState(false);
   const activeTexture = useSelector((state) => state.cubes.texture);
+  const [activeTextureName, setActiveTextureName] = useState("");
   const { dirt, grass, glass, log, wood, cobblestone, strippedLog } = useKeyboard();
 
   useEffect(() => {
@@ -42,13 +43,42 @@ export default function TextureSelector() {
     const pressedTexture = Object.entries(textures).find(([, v]) => v);
     if (pressedTexture) {
       dispatch(setTexture(pressedTexture[0]));
+
+      switch (pressedTexture[0]) {
+        case "dirt":
+          setActiveTextureName("Dirt");
+          break;
+        case "grass":
+          setActiveTextureName("Grass");
+          break;
+        case "glass":
+          setActiveTextureName("Glass");
+          break;
+        case "log":
+          setActiveTextureName("Log");
+          break;
+        case "wood":
+          setActiveTextureName("Wood");
+          break;
+        case "cobblestone":
+          setActiveTextureName("Cobblestone");
+          break;
+        case "strippedLog":
+          setActiveTextureName("Stripped Log");
+          break;
+        default:
+          setActiveTextureName("");
+          break;
+      }
     }
   }, [dirt, grass, glass, log, wood, cobblestone, strippedLog, setTexture]);
+
+  console.log("activeTextureName", activeTextureName);
 
   useEffect(() => {
     const visibilityTimeout = setTimeout(() => {
       setVisible(false);
-    }, 750);
+    }, 1000);
     setVisible(true);
 
     return () => {
@@ -59,16 +89,21 @@ export default function TextureSelector() {
   return (
     visible && (
       <div className="absolute top-[50%] right-[50%] translate-x-[25%] translate-y-[450%] scale-[5]">
+        <div>
+          <p className="text-[4px] h-2 m-2">{activeTextureName}</p>
+        </div>
         <div className="grid grid-cols-7 bg-slate-500 border border-zinc-800 rounded-sm">
           {Object.entries(images).map(([k, src]) => (
-            <img
-              key={k}
-              src={src}
-              alt={k}
-              className={`bg-slate-300 w-3 h-3 border-t-[0.2px] border-b-[0.2px] border-r-[0.05px] border-l-[0.05px] border-slate-500 rounded-sm ${
-                k === activeTexture ? "scale-125" : ""
-              }`}
-            />
+            <>
+              <img
+                key={k}
+                src={src}
+                alt={k}
+                className={`bg-slate-300 w-3 h-3 border-t-[0.2px] border-b-[0.2px] border-r-[0.05px] border-l-[0.05px] border-slate-500 rounded-sm ${
+                  k === activeTexture ? "scale-125" : ""
+                }`}
+              />
+            </>
           ))}
         </div>
       </div>
