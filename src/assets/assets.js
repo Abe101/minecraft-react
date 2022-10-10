89b93805b5d7aa8917538ba91e2c9c8f3d5dba7d
Abe101 <cyrusrohail101@gmail.com>
2022-10-10 20:36:45 +0500

    Add missing blocks and add faces and blocks to assets file

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -7,6 +7,22 @@ import woodImg from "./faces/plank.png";
 import strippedLogImg from "./faces/stripped_log.png";
 import cobblestoneImg from "./faces/cobblestone.png";
 import bedrockImg from "./faces/bedrock.png";
+import blackWoolImg from "./faces/black_wool.png";
+import whiteWooldImg from "./faces/white_wool.png";
+import blueWoolImg from "./faces/blue_wool.png";
+import brownWoolImg from "./faces/brown_wool.png";
+import cyanWoolImg from "./faces/cyan_wool.png";
+import grayWoolImg from "./faces/gray_wool.png";
+import greenWoolImg from "./faces/green_wool.png";
+import lightBlueWoolImg from "./faces/light_blue_wool.png";
+import lightGrayWoolImg from "./faces/light_gray_wool.png";
+import limeWoolImg from "./faces/lime_wool.png";
+import magentaWoolImg from "./faces/magenta_wool.png";
+import orangeWoolImg from "./faces/orange_wool.png";
+import pinkWoolImg from "./faces/pink_wool.png";
+import purpleWoolImg from "./faces/purple_wool.png";
+import redWoolImg from "./faces/red_wool.png";
+import yellowWoolImg from "./faces/yellow_wool.png";
 
 // Blocks
 import dirtBlock from "./blocks/dirt.png";
@@ -16,6 +32,22 @@ import logBlock from "./blocks/wood.png";
 import woodBlock from "./blocks/planks.png";
 import strippedLogBlock from "./blocks/stripped_wood.png";
 import cobblestoneBlock from "./blocks/cobblestone.png";
+import blackWoolBlock from "./blocks/black_wool.png";
+import whiteWooldBlock from "./blocks/white_wool.png";
+import blueWoolBlock from "./blocks/blue_wool.png";
+import brownWoolBlock from "./blocks/brown_wool.png";
+import cyanWoolBlock from "./blocks/cyan_wool.png";
+import grayWoolBlock from "./blocks/gray_wool.png";
+import greenWoolBlock from "./blocks/green_wool.png";
+import lightBlueWoolBlock from "./blocks/light_blue_wool.png";
+import lightGrayWoolBlock from "./blocks/light_gray_wool.png";
+import limeWoolBlock from "./blocks/lime_wool.png";
+import magentaWoolBlock from "./blocks/magenta_wool.png";
+import orangeWoolBlock from "./blocks/orange_wool.png";
+import pinkWoolBlock from "./blocks/pink_wool.png";
+import purpleWoolBlock from "./blocks/purple_wool.png";
+import redWoolBlock from "./blocks/red_wool.png";
+import yellowWoolBlock from "./blocks/yellow_wool.png";
 
 export { dirtImg, grassImg, glassImg, logImg, woodImg, strippedLogImg, cobblestoneImg, bedrockImg };
 export {
