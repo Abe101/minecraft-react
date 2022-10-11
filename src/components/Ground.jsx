06ea655bd9f89d2fb2862e8268cbea7273893126
Abe101 <cyrusrohail101@gmail.com>
2022-10-11 11:32:02 +0500

    Fix blocks placing above already placed block on ground

--- src/components/Ground.jsx
+++ src/components/Ground.jsx
@@ -1,11 +1,12 @@
 import { usePlane } from "@react-three/cannon";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { groundTexture } from "../assets/textures";
 import { addCube } from "../store/slices/cubeSlice";
 
 export default function Ground() {
   const dispatch = useDispatch();
+  const activeCubes = useSelector((state) => state.cubes.cubes);
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
     position: [0, -0.5, 0],
@@ -17,6 +18,13 @@ export default function Ground() {
       onClick={(e) => {
         e.stopPropagation();
         const [x, y, z] = Object.values(e.point).map((val) => Math.ceil(val));
+        const activePosition = activeCubes.map((cube) => {
+          const [X, Y, Z] = cube.pos;
+          if (X === x && Y === y && Z === z) {
+            return cube.pos;
+          }
+        });
+        if (JSON.stringify(activePosition.flat()) === JSON.stringify([x, y, z])) return;
         dispatch(addCube({ X: x, Y: y, Z: z }));
       }}>
       <planeGeometry attach="geometry" args={[200, 200]} />
